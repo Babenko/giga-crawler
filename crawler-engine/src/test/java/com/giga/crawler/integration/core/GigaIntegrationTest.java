034157d5a2f78d27efd637862f201eca7024fe97
Semen <babenkosemen@gmail.com>
2017-03-12 01:01:34 +0200

    Modify test

--- crawler-engine/src/test/java/com/giga/crawler/integration/core/GigaIntegrationTest.java
+++ crawler-engine/src/test/java/com/giga/crawler/integration/core/GigaIntegrationTest.java
@@ -2,13 +2,14 @@ package com.giga.crawler.integration.core;
 
 import com.giga.crawler.core.Giga;
 import com.giga.crawler.model.element.Element;
-import org.hamcrest.Matchers;
+import com.giga.crawler.model.element.ElementName;
 import org.junit.Test;
 import util.ResourceLoader;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Sem Babenko
@@ -22,7 +23,13 @@ public class GigaIntegrationTest extends IntegrationTest {
 
         Element root = Giga.of(ResourceLoader.loadFile(SIMPLE_HTML)).getRoot();
         List<Element> children = root.getChildren();
-        assertThat(children.size(), Matchers.equalTo(2));
+        assertThat(children.size(), equalTo(2));
+        Element body = children.get(1);
+        assertThat(body.getName(), equalTo(ElementName.BODY));
+        List<Element> bodyChildren = body.getChildren();
+        assertThat(bodyChildren.size(), equalTo(1));
+        Element div = bodyChildren.get(0);
+        assertThat(div.getName(), equalTo(ElementName.DIV));
 
     }
 
