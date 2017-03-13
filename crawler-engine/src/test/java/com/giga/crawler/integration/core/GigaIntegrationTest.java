ddc76b4089addb1e00de77627d5095f37dbb47a0
Semen <babenkosemen@gmail.com>
2017-03-13 22:26:44 +0200

    Prepare test for element payload

--- crawler-engine/src/test/java/com/giga/crawler/integration/core/GigaIntegrationTest.java
+++ crawler-engine/src/test/java/com/giga/crawler/integration/core/GigaIntegrationTest.java
@@ -65,4 +65,9 @@ public class GigaIntegrationTest extends IntegrationTest {
         assertNull(span.getNext());
     }
 
+    @Test
+    public void testSimpleElementPayload() throws Exception {
+        
+    }
+
 }
